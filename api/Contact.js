0660a729196c6ca1d2e0ab4a1922f333fa2fa0c6
Ankit Raj <ankit5242raj@gmail.com>
2025-06-20 20:24:06 +0530

    Update Contact.js
    Signed-off-by: Ankit Raj <ankit5242raj@gmail.com>

--- api/Contact.js
+++ api/Contact.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Only POST allowed" });
-
+  if (req.method !== "POST") return res.status(405).end();
   const { name, email, message } = req.body;
-  console.log("Contact:", name, email, message);
-  res.status(200).json({ message: "Form submitted successfully" });
+  console.log(name, email, message);
+  res.status(200).json({ message: "Form submitted successfully!" });
 }
