d777860f4c42e942c3ef26183df092233d82aa81
Ankit Raj <ankit5242raj@gmail.com>
2025-06-22 02:34:10 +0530

    Update script.js
    Signed-off-by: Ankit Raj <ankit5242raj@gmail.com>

--- script.js
+++ script.js
@@ -179,23 +179,3 @@ document.addEventListener('DOMContentLoaded', animateAboutLines);
 window.addEventListener('scroll', animateAboutLines);
 
 
-//api section for get in touch
-document.getElementById("contactForm").addEventListener("submit", async function (e) {
-  e.preventDefault();
-  const form = e.target;
-  const data = {
-    name: form.name.value,
-    email: form.email.value,
-    message: form.message.value,
-  };
-
-  const res = await fetch("/api/contact", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
-
-  const result = await res.json();
-  alert(result.message || "Sent!");
-  form.reset();
-});
